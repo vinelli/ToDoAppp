1b43d37e55b63ebc8b1eb72edff063f961b998b4
vinelli <vinnievinelli@gmail.com>
2021-06-22 17:24:29 +0100

    initial commit

---
+++ app/src/main/java/com/kanayo/todoappp/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.kanayo.todoappp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var usernameDisplay: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        usernameDisplay = findViewById(R.id.usernameDisplay)
+
+        usernameDisplay.text = intent.getStringExtra("name")
+    }
+}
